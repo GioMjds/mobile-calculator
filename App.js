d33d600c44d80f3861289b5e228aa7f56828254f
Gio-Majadas <sensei.makima55@gmail.com>
2024-09-07 17:08:08 +0800

    First commit, tas kagaguhan pa

--- App.js
+++ App.js
@@ -120,9 +120,9 @@ const App = () => {
     const sub = value.split(",");
     let formatted = "";
     if (sub.length > 1) {
-      formatted = sub[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ") + "," + sub[1];
+      formatted = sub[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "," + sub[1];
     } else {
-      formatted = sub[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+      formatted = sub[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     }
     return formatted;
   }
@@ -144,6 +144,8 @@ const App = () => {
       "Sorry na agad",
       "Sorry na talaga",
       "Sorry na bhe",
+      "Mahal mo pa ba ako? ;(",
+      "Sana ako nalang ulit",
     ]
 
     return randomWords[Math.floor(Math.random() * randomWords.length)];
@@ -151,75 +153,86 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.screens}>
-        <Text style={styles.firstScreen}>{calc.firstOperand !== null ? `${format(calc.firstOperand)} ${calc.operator}` : ""}</Text>
-        <Text style={[styles.secondScreen, calc.secondOperand !== null && calc.secondOperand.length > 9 ? styles.secondScreenSmall : styles.secondScreenNormal]}>{format(calc.secondOperand)}</Text>
-      </View>
-      <View style={styles.inputsWrapper}>
-        <TouchableOpacity style={styles.featuredButton} onPress={() => handleClear()}>
-          <Text style={styles.featuredButtonText}>C</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.featuredButton} onPress={() => handlePlusMinus()}>
-          <Text style={styles.featuredButtonText}>+/-</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.featuredButton} onPress={() => handlePercent()}>
-          <Text style={styles.featuredButtonText}>%</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.featuredButton} onPress={() => handleOperator('÷')}>
-          <Text style={styles.featuredButtonText}>÷</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleNumber('7')}>
-          <Text style={styles.buttonText}>7</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleNumber('8')}>
-          <Text style={styles.buttonText}>8</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleNumber('9')}>
-          <Text style={styles.buttonText}>9</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.featuredButton} onPress={() => handleOperator('*')}>
-          <Text style={styles.featuredButtonText}>*</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleNumber('4')}>
-          <Text style={styles.buttonText}>4</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleNumber('5')}>
-          <Text style={styles.buttonText}>5</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleNumber('6')}>
-          <Text style={styles.buttonText}>6</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.featuredButton} onPress={() => handleOperator('-')}>
-          <Text style={styles.featuredButtonText}>-</Text>
-        </TouchableOpacity>
-                <TouchableOpacity style={styles.button} onPress={() => handleNumber('1')}>
-          <Text style={styles.buttonText}>1</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleNumber('2')}>
-          <Text style={styles.buttonText}>2</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleNumber('3')}>
-          <Text style={styles.buttonText}>3</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.featuredButton} onPress={() => handleOperator('+')}>
-          <Text style={styles.featuredButtonText}>+</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleNumber(',')}>
-          <Text style={styles.buttonText}>,</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleNumber('0')}>
-          <Text style={styles.buttonText}>0</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={() => handleDelete()}>
-          <Text style={styles.buttonText}>Del</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.featuredButton} onPress={() => handleCompute()}>
-          <Text style={styles.featuredButtonText}>=</Text>
-        </TouchableOpacity>
-
+      <View style={styles.calculator}>
+        <View style={styles.screens}>
+          <Text style={styles.firstScreen}>{calc.firstOperand !== null ? `${format(calc.firstOperand)} ${calc.operator}` : ""}</Text>
+          <Text style={[styles.secondScreen, calc.secondOperand !== null && calc.secondOperand.length > 9 ? styles.secondScreenSmall : styles.secondScreenNormal]}>{format(calc.secondOperand)}</Text>
+        </View>
+        <View style={styles.inputsWrapper}>
+          <View style={styles.buttonRow}>
+            <TouchableOpacity style={styles.featuredButton} onPress={() => handleClear()}>
+              <Text style={styles.featuredButtonText}>C</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.featuredButton} onPress={() => handlePlusMinus()}>
+              <Text style={styles.featuredButtonText}>+/-</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.featuredButton} onPress={() => handlePercent()}>
+              <Text style={styles.featuredButtonText}>%</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.featuredButton} onPress={() => handleOperator('÷')}>
+              <Text style={styles.featuredButtonText}>÷</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.buttonRow}>
+            <TouchableOpacity style={styles.button} onPress={() => handleNumber('7')}>
+              <Text style={styles.buttonText}>7</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => handleNumber('8')}>
+              <Text style={styles.buttonText}>8</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => handleNumber('9')}>
+              <Text style={styles.buttonText}>9</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.featuredButton} onPress={() => handleOperator('*')}>
+              <Text style={styles.featuredButtonText}>*</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.buttonRow}>
+            <TouchableOpacity style={styles.button} onPress={() => handleNumber('4')}>
+              <Text style={styles.buttonText}>4</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => handleNumber('5')}>
+              <Text style={styles.buttonText}>5</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => handleNumber('6')}>
+              <Text style={styles.buttonText}>6</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.featuredButton} onPress={() => handleOperator('-')}>
+              <Text style={styles.featuredButtonText}>-</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.buttonRow}>
+            <TouchableOpacity style={styles.button} onPress={() => handleNumber('1')}>
+              <Text style={styles.buttonText}>1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => handleNumber('2')}>
+              <Text style={styles.buttonText}>2</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => handleNumber('3')}>
+              <Text style={styles.buttonText}>3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.featuredButton} onPress={() => handleOperator('+')}>
+              <Text style={styles.featuredButtonText}>+</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.buttonRow}>
+            <TouchableOpacity style={styles.button} onPress={() => handleNumber(',')}>
+              <Text style={styles.buttonText}>,</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => handleNumber('0')}>
+              <Text style={styles.buttonText}>0</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => handleDelete()}>
+              <Text style={styles.buttonText}>Del</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.featuredButton} onPress={() => handleCompute()}>
+              <Text style={styles.featuredButtonText}>=</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     </View>
-  );
+  )
 };
 
 const styles = StyleSheet.create({
@@ -227,45 +240,79 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#333',
+    backgroundColor: '#f5f5f5',
+  },
+  calculator: {
+    width: 420,
+    height: 860,
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+      elevation: 5,
+    },
   },
   screens: {
+    // padding: 10,
+    // display: 'flex',
+    // flexDirection: 'column',
+    // justifyContent: 'space-between',
+    // backgroundColor: '#333',
+    // color: 'whitesmoke',
+    // minHeight: 50,
+    // borderRadius: 5,
     padding: 10,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    marginBottom: 20,
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 16,
+    height: '100%',
+    borderRadius: 8,
+    backgroundColor: '#333',
+    color: 'whitesmoke',
   },
   firstScreen: {
-    fontSize: 24,
-    color: '#333',
+    fontSize: 16,
+    color: '#fff',
   },
   secondScreen: {
     fontSize: 36,
-    color: '#333',
+    color: '#fff',
   },
   secondScreenNormal: {
-    fontSize: 36,
+    fontSize: 30,
   },
   secondScreenSmall: {
-    fontSize: 24,
+    fontSize: 30,
   },
   inputsWrapper: {
-    flexDirection: 'row',
+    display: 'flex',
     flexWrap: 'wrap',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    marginTop: 50,
+    padding: 1,
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
   },
   button: {
-    width: 80,
-    height: 80,
+    width: 70,
+    height: 55,
     borderRadius: 10,
     backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
-    margin: 5,
+    margin: 2,
+    borderWidth: 1,
+    borderColor: '#ddd',
   },
   featuredButton: {
-    width: 80,
-    height: 80,
+    width: 70,
+    height: 55,
     borderRadius: 10,
     backgroundColor: '#ff6b00',
     justifyContent: 'center',
